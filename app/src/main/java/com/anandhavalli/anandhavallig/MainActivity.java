cc95713e62b00ff9a11b68e56be1dd5aff6d77db
Anandhavalli G <anandhavalli004@gmail.com>
2018-06-23 17:07:57 +0530

    Added play store link

--- app/src/main/java/com/anandhavalli/anandhavallig/MainActivity.java
+++ app/src/main/java/com/anandhavalli/anandhavallig/MainActivity.java
@@ -1,7 +1,13 @@
 package com.anandhavalli.anandhavallig;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.link)
+    public void okAction(View view) {
+        launchPlayStore();
+    }
+
+    private void launchPlayStore() {
+        Uri uri = Uri.parse("https://play.google.com/store/apps/developer?id=Anandhavalli+G");
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        try {
+            startActivity(goToMarket);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
